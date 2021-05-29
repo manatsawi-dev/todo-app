4eaf646c1fd3f548d9db1d44482bea94ee9e70f4
manatsawi-idev <61825272+manatsawi-dev@users.noreply.github.com>
2021-05-29 14:55:06 +0700

    init: setup

---
+++ src/screens/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoScreen = () => {
+  return (
+    <div>
+      <h1>TODO APP</h1>
+    </div>
+  );
+};
+
+TodoScreen.propTypes = {};
+
+export default TodoScreen;
